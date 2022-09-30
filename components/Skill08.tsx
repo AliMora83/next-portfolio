f6281fb9d1bf5f61760c1ae8f45b0f1cccab1b92
Daurius Swarts <ali@openmindi.co.za>
2022-10-01 01:06:44 +0200

    nuxt update

--- components/Skill08.tsx
+++ components/Skill08.tsx
@@ -19,7 +19,7 @@ export default function Skill01(directionLeft: any) {
         className="rounded-full border border-gray-500 object-cover w-24 h-24
         xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out
         md:w-28 md:h-28"
-        src="/NUXT.WEBP" alt="image" />
+        src="/nuxt.webp" alt="image" />
 
         <div className="absolute opacity-0 rounded-full border border-gray-500 object-cover w-24 h-24
         xl:w-32 xl:h-32 filter group-hover:opacity-80 transition duration-300 ease-in-out
