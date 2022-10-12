1f90f9bb447fc744e7a00c5ebd480d7804bdc9f7
Daurius Swarts <ali@openmindi.co.za>
2022-10-12 14:13:34 +0200

    Yarn to NPM

--- components/Skill08.tsx
+++ components/Skill08.tsx
@@ -16,12 +16,12 @@ export default function Skill08(directionLeft: any) {
         }}
         transition={{duration:1}}
         whileInView={{opacity:1, x:0}}
-        className="rounded-full border border-gray-500 object-cover w-24 h-24
+        className="rounded-full border border-gray-500 object-cover w-16 h-16
         xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out
         md:w-28 md:h-28"
         src="/nuxt.webp" alt="image" />
 
-        <div className="absolute opacity-0 rounded-full border border-gray-500 object-cover w-24 h-24
+        <div className="absolute opacity-0 rounded-full border border-gray-500 object-cover w-16 h-16
         xl:w-32 xl:h-32 filter group-hover:opacity-80 transition duration-300 ease-in-out
         md:w-28 md:h-28 group-hover:bg-white z-0">
             <div className='flex items-center justify-center h-full'>
