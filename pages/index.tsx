31d5df4e21ff89aa784c51127032b3018725ff35
Daurius Swarts <ali@openmindi.co.za>
2022-11-22 13:09:38 +0200

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const Home = () => {
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0
     scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]" >
       <Head>
-        <title>Ali Mora X</title>
+        <title>Ali Mora</title>
         {/* <html lang="en-us"/> */}
         <meta name="description" content="Ali Mora Portfolio" />
         <link rel="icon" href="/omi.png" />
